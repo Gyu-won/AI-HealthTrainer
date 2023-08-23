872ec917ec4466cd791a7606214ec681158632b8
Gyu-won <milestogo0905@naver.com>
2023-08-23 12:05:14 +0900

    design(homepage.css, hompage.html, hompage.js): Add our pictures in homepage

--- AI_HealthTrainer/static/javascripts/homepage.js
+++ AI_HealthTrainer/static/javascripts/homepage.js
@@ -1,9 +1,27 @@
-const hamburger = document.querySelector(".hamburger");
-const closeIcon = document.querySelector(".close-icon");
-const menu = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const closeIcon = document.querySelector(".close-icon");
+  const menu = document.querySelector(".menu");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  closeIcon.classList.toggle("active");
-  menu.classList.toggle("active");
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    closeIcon.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
+
+  closeIcon.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    closeIcon.classList.toggle("active");
+    menu.classList.toggle("active");
+  });
+
+  // Tambahkan listener click untuk setiap link menu
+  const menuLinks = document.querySelectorAll(".menu a");
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      closeIcon.classList.remove("active");
+      menu.classList.remove("active");
+    });
+  });
 });
